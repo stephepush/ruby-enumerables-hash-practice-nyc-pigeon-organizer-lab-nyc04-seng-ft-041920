8eae7009f382a9a3b11814a56978ce9a723182a7
Stephen Peters <7speter@gmail.com>
2020-05-10 15:30:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,10 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
 	organized_data = {} # Used to store the names of the pigeons and assign values
-	
+
 	 # Iterates through top layer of Hash
 	data.each do|color_gender_lives, attribute_hash|
-		
+
 		# Iterate through middle layer of hash
 		attribute_hash.each do |attribute, name_array|
 
@@ -14,7 +14,7 @@ def nyc_pigeon_organizer(data)
 				if organized_data[name] == nil # If this statement returns true by being false
 					organized_data[name] = {} # This creates a hash with a key of ["name"] and a value of an empty hash
 				end
-			
+
 				if organized_data[name][color_gender_lives] == nil # Reserved for making the array if the attributes do not exist
 					organized_data[name][color_gender_lives] = []
 				end
